2d72f6f12eb0c049781514d6e76675429153cb05
jcstange <jcstange@gmail.com>
2023-09-28 11:21:03 +0300

    Initial Commit

---
+++ Clipboard History/Clipboard_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_HistoryApp.swift
+//  Clipboard History
+//
+//  Created by Joao Cesar Stange on 28.9.2023.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
