37c668ba527afed34f4080e6d602c8305d783175
jcstange <jcstange@gmail.com>
2023-10-02 14:33:45 +0300

    Issue #7: The main window is supressed

---
+++ Clipboard History/AppDelegate.swift
@@ -0,0 +1,36 @@
+import Cocoa
+import SwiftUI
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var statusItem: NSStatusItem!
+    var popover: NSPopover!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if let window = NSApplication.shared.windows.first {
+            window.close()
+        }
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "circle", accessibilityDescription: nil)
+            button.action = #selector(togglePopover(_:))
+        }
+        
+        let contentView = ClipboardView(viewModel: ClipboardViewModel())
+        popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 600)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+    }
+
+    @objc func togglePopover(_ sender: Any?) {
+        if let button = statusItem.button {
+            if popover.isShown {
+                popover.performClose(sender)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
+        }
+    }
+}
+
--- Clipboard History/Clipboard_HistoryApp.swift
+++ Clipboard History/Clipboard_HistoryApp.swift
@@ -5,46 +5,6 @@ import Cocoa
 struct Clipboard_HistoryApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
-            ClipboardView(viewModel: ClipboardViewModel())
-                .onAppear {
-                    NSApp.setActivationPolicy(.prohibited)
-                }
-        }
+        WindowGroup {}
     }
 }
-
-
-
-
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    var statusItem: NSStatusItem!
-    var popover: NSPopover!
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        if let button = statusItem.button {
-            button.image = NSImage(systemSymbolName: "circle", accessibilityDescription: nil)
-            button.action = #selector(togglePopover(_:))
-        }
-        
-        let contentView = ClipboardView(viewModel: ClipboardViewModel())
-        popover = NSPopover()
-        popover.contentSize = NSSize(width: 400, height: 600)
-        popover.behavior = .transient
-        popover.contentViewController = NSHostingController(rootView: contentView)
-    }
-
-    @objc func togglePopover(_ sender: Any?) {
-        if let button = statusItem.button {
-            if popover.isShown {
-                popover.performClose(sender)
-            } else {
-                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
-            }
-        }
-    }
-}
-
